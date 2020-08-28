4cd62b2b3fa4d44d3b9cdd0b5897fe5dc1bef267
fpichot <franck.pichot@etudiant.univ-brest.fr>
2020-08-28 12:12:48 +0200

    install obitools

---
+++ install.sh
@@ -0,0 +1,18 @@
+﻿#install conda
+cd ./Users/franck/Downloads/
+bash Anaconda3-2020.07-MacOSX-x86_64.sh
+#fermeture du logiciel
+conda config --set auto_activate_base false
+conda --help
+
+#creer nouvel environnement obitools
+ENVYAML=/Users/franck/Projects/installation_obitools/installation_obitools/environnements/obitools_env_conda.yaml
+conda env create -f $ENVYAML
+## To activate this environment, use
+#     $ conda activate obitools
+# To deactivate an active environment, use
+#     $ conda deactivate
+#Test des commandes
+
+#Supprimer un environnement
+conda env remove --name obitools
